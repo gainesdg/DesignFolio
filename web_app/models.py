4ceda8d15f3f3b5fac5c265d14b2513ff56d8ef6
JoeSubbi <joe.subbiani@gmail.com>
2020-03-13 15:12:10 +0000

    professions and tags models created and can be populated

--- web_app/models.py
+++ web_app/models.py
@@ -14,7 +14,7 @@ class Profession(models.Model):
     def __str__(self):
         return self.name
 
-
+"""
 class User(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     user_name = models.CharField(max_length=128, unique=True)
@@ -27,24 +27,27 @@ class User(models.Model):
 
     def __str__(self):
         return self.user_name
-
-
+"""
+"""
 class Section(models.Model):
     name = models.CharField(max_length=128, unique=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
+  #user = models.ForeignKey(User, on_delete=models.CASCADE)
+"""
 
 # Create your models here.
 class Tags(models.Model):
     name = models.CharField(max_length=128)
     profession = models.ForeignKey(Profession, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
 
+"""
 class Posts(models.Model):
-    post_id = models.IntegerField()
-    category = models.ForeignKey(Section, on_delete=models.CASCADE)
+    post_id = models.IntegerField(unique=True)
+    section = models.ForeignKey(Section, on_delete=models.CASCADE)
     title = models.CharField(max_length=128)
     descriptiuon = models.CharField(max_length=512)
     likes = models.IntegerField(default=0)
     tags = models.ManyToManyField(Tags)
-
+"""
